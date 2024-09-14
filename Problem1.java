243dd1dc7812932b977d4bea45602fae6cf6e470
YMXNVWIWNVNMXY <jnbui106@student.hbuhsd.edu>
2024-09-13 18:25:59 -0700

    Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -2,6 +2,10 @@ public class Problem1
 {
   public static void main(String[] args)
   {
-    // insert code here
+  double numerator = 5;
+  double denominator = 20;
+   System.out.println("numerator: " + numerator);
+   System.out.println("denominator: " + denominator);
+   System.out.println("The decimal value is: " + (numerator / denominator));
   }
 }
